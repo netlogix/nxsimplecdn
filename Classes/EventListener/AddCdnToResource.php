5e0e884ca1c8c2ff920e1abd879e0bb901f7d47c
Tim Weisenberger <tim.weisenberger@netlogix.de>
2024-01-29 09:20:29 +0100

    feat: Update extension to TYPO3 v12

--- Classes/EventListener/AddCdnToResource.php
+++ Classes/EventListener/AddCdnToResource.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Netlogix\Nxsimplecdn\EventListener;
@@ -6,6 +7,7 @@ namespace Netlogix\Nxsimplecdn\EventListener;
 use InvalidArgumentException;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\UriInterface;
+use TYPO3\CMS\Core\Http\ApplicationType;
 use TYPO3\CMS\Core\Http\Uri;
 use TYPO3\CMS\Core\Resource\Driver\DriverInterface;
 use TYPO3\CMS\Core\Resource\Driver\LocalDriver;
@@ -16,35 +18,42 @@ use TYPO3\CMS\Core\Resource\ResourceInterface;
 use TYPO3\CMS\Core\Resource\ResourceStorageInterface;
 use TYPO3\CMS\Core\Site\Entity\Site;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Service\EnvironmentService;
 
 class AddCdnToResource
 {
-
     public function __invoke(GeneratePublicUrlForResourceEvent $event): void
     {
-        $environmentService = GeneralUtility::makeInstance(EnvironmentService::class);
         $driver = $event->getDriver();
         $resource = $event->getResource();
-        if (! ($driver instanceof LocalDriver) || $environmentService->isEnvironmentInBackendMode()) {
+        if (
+            !($driver instanceof LocalDriver) ||
+            ApplicationType::fromRequest($GLOBALS['TYPO3_REQUEST'])->isBackend()
+        ) {
             return;
         }
 
-        if (($resource instanceof File || $resource instanceof ProcessedFile) && ($resource->getStorage(
-                )->getCapabilities(
-                ) & ResourceStorageInterface::CAPABILITY_PUBLIC) == ResourceStorageInterface::CAPABILITY_PUBLIC) {
-            $event->setPublicUrl($this->addCdnPrefixToUrl($resource, $driver));
+        if (!$resource instanceof File && !$resource instanceof ProcessedFile) {
+            return;
         }
+
+        if (($resource->getStorage()->getCapabilities(
+        ) & ResourceStorageInterface::CAPABILITY_PUBLIC) !== ResourceStorageInterface::CAPABILITY_PUBLIC) {
+            return;
+        }
+
+        $event->setPublicUrl($this->addCdnPrefixToUrl($resource, $driver));
     }
 
     private function addCdnPrefixToUrl(ResourceInterface $resourceObject, DriverInterface $driver): string
     {
         $publicUrl = $driver->getPublicUrl($resourceObject->getIdentifier());
         $cdnBaseUrl = $this->getCdnBase();
-        if (! $resourceObject instanceof ProcessedFile) {
+        if (!$resourceObject instanceof ProcessedFile) {
             $publicUrl = GeneralUtility::createVersionNumberedFilename($publicUrl);
         }
-        return (string) (new Uri($publicUrl))->withScheme('https')->withHost($cdnBaseUrl->getHost());
+
+        return (string) (new Uri($publicUrl))->withScheme('https')
+            ->withHost($cdnBaseUrl->getHost());
     }
 
     private function getCdnBase(): UriInterface
@@ -52,9 +61,10 @@ class AddCdnToResource
         $request = $this->getRequest();
         $site = $request->getAttribute('site');
         assert($site instanceof Site);
+
         try {
             return new Uri($site->getAttribute('cdnBase'));
-        } catch (InvalidArgumentException $exception) {
+        } catch (InvalidArgumentException) {
             return $site->getBase();
         }
     }
--- ext_localconf.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('TYPO3_MODE') or die();
-
-(function () {
-    if (TYPO3_MODE == 'FE') {
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['typoLink_PostProc']['tx_nxsimplecdn'] = \Netlogix\Nxsimplecdn\SignalSlot\ResourceStorage::class . '->addFileCacheTagForTypolink';
-    }
-})();
