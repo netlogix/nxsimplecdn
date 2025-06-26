37aed96d0df2988226483313deaa4bd111735079
Sascha Nowak <sascha.nowak@netlogix.de>
2025-06-26 21:10:34 +0200

    feat: add backend content security policy for cdn domain

---
+++ Configuration/ContentSecurityPolicies.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Directive;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Mutation;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\MutationCollection;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\MutationMode;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Scope;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\SourceScheme;
+use TYPO3\CMS\Core\Security\ContentSecurityPolicy\UriValue;
+use TYPO3\CMS\Core\Type\Map;
+
+if (getenv('CDN_BASE') === false || getenv('CDN_BASE') === '') {
+    return Map::fromEntries();
+}
+
+return Map::fromEntries(
+    [
+        Scope::backend(),
+        new MutationCollection(
+            new Mutation(
+                MutationMode::Extend,
+                Directive::DefaultSrc,
+                SourceScheme::data,
+                new UriValue(sprintf('https://%s', getenv('CDN_BASE')))
+            ),
+            new Mutation(
+                MutationMode::Extend,
+                Directive::ImgSrc,
+                SourceScheme::data,
+                new UriValue(sprintf('https://%s', getenv('CDN_BASE')))
+            ),
+        ),
+    ]
+);
